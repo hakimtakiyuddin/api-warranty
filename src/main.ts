e44c46902220a849ba8b834dec32d06aefdf43e1
Abdullah Hakim <hakimtakiyuddin@Abdullahs-Mac-mini.local>
2024-12-23 13:54:32 +0800

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Warranty API')
+    .setDescription('The warranty API description')
+    .setVersion('1.0')
+    .addTag('warranty')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-warranty', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
